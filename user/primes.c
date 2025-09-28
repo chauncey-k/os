dbbf22e232ee7e05a10d310cc2e07880f84ca0ec
Chauncey <1073695360@qq.com>
2025-09-28 17:53:17 +0800

    add some in primes

--- user/primes.c
+++ user/primes.c
@@ -11,31 +11,48 @@ int
 main(int argc, char *argv[])
 {
     int p[2];
-    int num = 2;
 
-    if(pipe(p) < 0){
-        // 创建失败
+    if(pipe(p) < 0) {
         fprintf(2, "pipe failed\n");
         exit(1);
     }
 
-    close(p[0]); // 关闭读端
-    for(num = 2; num <= 35; num++){
-        write(p[1], &num, sizeof(int)); // 向管道写入数字
+    for(int num = 2; num <= 35; num++) {
+        write(p[1], &num, sizeof(int)); // write source to pipe
     }
 
     int pid = fork();
-    if(pid < 0){
-        // 创建子进程失败
+    if(pid < 0) {
         fprintf(2, "fork failed\n");
         exit(1);
-    } else if(pid == 0){
+    } else if(pid == 0) {
         int prime;
-        // 子进程
-        if (read(p[0], &prime, sizeof(int)) == 0);{ // 如果读取到最后一个数据
-            close(p[0]);
-            exit(0);
+        int num;
+        int next_p[2];
 
+        if (pipe(next_p) < 0) {
+            fprintf(2, "pipe failed\n");
+            exit(1);
+        }
+        // child process
+        close(0);   // close stdin fd 0
+        dup(p[0]);  // read data from pipe
+        read(p[0], &prime, sizeof(int)); //read first data as prime
+        print("prime %d\n, prime") // print prime
+
+        close(p[0]); // close parent->chlid pipe read port
+
+        while (read(p[0], &num, sizeof(int) > 0)) {
+            if (num % prime != 0) {
+                write(next_p[1], &num, sizeof(int));
+            }
+        }
+
+        close(next_p[0]);
+        close(next_p[1]);
     }
+
+
+    close(p[0]); // shutdown read port
     exit(0);
 }
